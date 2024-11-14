21693c140002d39a4988c0169656dfa08614b377
Dominik Schöler <dominik.schoeler@makandra.de>
2024-11-14 10:01:38 +0100

    Outdent cli.rb

--- lib/gurney/cli.rb
+++ lib/gurney/cli.rb
@@ -14,90 +14,88 @@ module Gurney
     def self.run(cmd_parameter=[])
       options = Gurney::CLI::OptionParser.parse(cmd_parameter)
 
-      begin
-        if options.hook
-          g = Git.bare(ENV['GIT_DIR'] || Dir.pwd)
-        else
-          unless Dir.exist? './.git'
-            raise Gurney::Error.new('Must be run within a git repository')
-          end
-          g = Git.open('.')
+      if options.hook
+        g = Git.bare(ENV['GIT_DIR'] || Dir.pwd)
+      else
+        unless Dir.exist? './.git'
+          raise Gurney::Error.new('Must be run within a git repository')
         end
-        config_file = MAIN_BRANCHES.find do |branch|
-          file = read_file(g, options.hook, branch, options.config_file)
-          break file if file
-        end
-        if !config_file && options.hook
-          # dont run as a hook with no config
-          exit 0
-        end
-        config_file ||= '---'
-        config = Gurney::Config.from_yaml(config_file)
+        g = Git.open('.')
+      end
+      config_file = MAIN_BRANCHES.find do |branch|
+        file = read_file(g, options.hook, branch, options.config_file)
+        break file if file
+      end
+      if !config_file && options.hook
+        # dont run as a hook with no config
+        exit 0
+      end
+      config_file ||= '---'
+      config = Gurney::Config.from_yaml(config_file)
 
-        options.branches ||= config&.branches
-        options.branches ||= config&.branches
-        options.api_token ||= config&.api_token
-        options.api_url ||= config&.api_url
-        options.project_id ||= config&.project_id
+      options.branches ||= config&.branches
+      options.branches ||= config&.branches
+      options.api_token ||= config&.api_token
+      options.api_url ||= config&.api_url
+      options.project_id ||= config&.project_id
 
-        if [options.project_id, options.branches, options.api_url, options.api_token].any?(&:nil?)
-          raise Gurney::Error.new("Either provide in a config file or set the flags for project id, branches, api url and api token")
-        end
+      if [options.project_id, options.branches, options.api_url, options.api_token].any?(&:nil?)
+        raise Gurney::Error.new("Either provide in a config file or set the flags for project id, branches, api url and api token")
+      end
 
-        branches = []
-        if options.hook || options.client_hook
-          # we get passed changed branches and refs via stdin
-          $stdin.each_line do |line|
-            regex = options.client_hook ? CLIENT_HOOK_STDIN_REGEX : HOOK_STDIN_REGEX
-            line.force_encoding(Encoding::UTF_8)
-            matches = line.match(regex)
-            if matches && matches[:new] != '0' * 40
-              if options.branches.include? matches[:ref]
-                branches << matches[:ref]
-              end
+      branches = []
+      if options.hook || options.client_hook
+        # we get passed changed branches and refs via stdin
+        $stdin.each_line do |line|
+          regex = options.client_hook ? CLIENT_HOOK_STDIN_REGEX : HOOK_STDIN_REGEX
+          line.force_encoding(Encoding::UTF_8)
+          matches = line.match(regex)
+          if matches && matches[:new] != '0' * 40
+            if options.branches.include? matches[:ref]
+              branches << matches[:ref]
             end
           end
-
-        else
-          current_branch = g.current_branch
-          unless options.branches.nil? || options.branches.include?(current_branch)
-            raise Gurney::Error.new('The current branch is not specified in the config.')
-          end
-          branches << current_branch
         end
 
-        branches.each do |branch|
-          dependencies = []
-
-          yarn_source = Gurney::Source::Yarn.new(yarn_lock: read_file(g, options.hook || options.client_hook, branch, 'yarn.lock'))
-          dependencies.concat yarn_source.dependencies || []
-
-          bundler_source = Gurney::Source::Bundler.new(gemfile_lock: read_file(g, options.hook || options.client_hook, branch, 'Gemfile.lock'))
-          dependencies.concat bundler_source.dependencies || []
-
-          ruby_version_source = Gurney::Source::RubyVersion.new(ruby_version: read_file(g, options.hook || options.client_hook, branch, '.ruby-version'))
-          dependencies.concat ruby_version_source.dependencies || []
-
-          dependencies.compact!
-
-          api = Gurney::Api.new(base_url: options.api_url, token: options.api_token)
-          api.post_dependencies(dependencies: dependencies, branch: branch, project_id: options.project_id)
-
-          dependency_counts = dependencies.group_by(&:ecosystem).map{|ecosystem, dependencies| "#{ecosystem}: #{dependencies.count}" }.join(', ')
-          puts "Gurney: reported dependencies (#{dependency_counts})"
+      else
+        current_branch = g.current_branch
+        unless options.branches.nil? || options.branches.include?(current_branch)
+          raise Gurney::Error.new('The current branch is not specified in the config.')
         end
-
-      rescue SystemExit
-      rescue Gurney::ApiError => e
-        puts "Gurney: api error".red
-        puts e.message.red
-      rescue Gurney::Error => e
-        puts "Gurney: error".red
-        puts e.message.red
-      rescue Exception => e
-        puts "Gurney: an unexpected error occurred".red
-        raise
+        branches << current_branch
       end
+
+      branches.each do |branch|
+        dependencies = []
+
+        yarn_source = Gurney::Source::Yarn.new(yarn_lock: read_file(g, options.hook || options.client_hook, branch, 'yarn.lock'))
+        dependencies.concat yarn_source.dependencies || []
+
+        bundler_source = Gurney::Source::Bundler.new(gemfile_lock: read_file(g, options.hook || options.client_hook, branch, 'Gemfile.lock'))
+        dependencies.concat bundler_source.dependencies || []
+
+        ruby_version_source = Gurney::Source::RubyVersion.new(ruby_version: read_file(g, options.hook || options.client_hook, branch, '.ruby-version'))
+        dependencies.concat ruby_version_source.dependencies || []
+
+        dependencies.compact!
+
+        api = Gurney::Api.new(base_url: options.api_url, token: options.api_token)
+        api.post_dependencies(dependencies: dependencies, branch: branch, project_id: options.project_id)
+
+        dependency_counts = dependencies.group_by(&:ecosystem).map{|ecosystem, dependencies| "#{ecosystem}: #{dependencies.count}" }.join(', ')
+        puts "Gurney: reported dependencies (#{dependency_counts})"
+      end
+
+    rescue SystemExit
+    rescue Gurney::ApiError => e
+      puts "Gurney: api error".red
+      puts e.message.red
+    rescue Gurney::Error => e
+      puts "Gurney: error".red
+        puts e.message.red
+    rescue Exception
+      puts "Gurney: an unexpected error occurred".red
+      raise
     end
 
     private
